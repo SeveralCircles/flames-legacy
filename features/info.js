5e7a49bc6387fc8c8af84c7d0091bc8c6ebd4f6b
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-05-07 18:25:25 -0400

    fixed for realsies

--- features/info.js
+++ features/info.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 module.exports = {
     wait: function(member, client, operation) {
-        let embed = new Discord.MessageEmbed
+        let embed = new Discord.MessageEmbed()
         .setAuthor("Please Wait", member.user.displayAvatarURL())
         .setTitle(member.displayName + ", please wait while your request is being proccessed.")
         .setDescription("This shouldn't take too long. If it does, your request may have encountered an error.")
