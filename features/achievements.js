fb243f5f62c6f9a12ea1b25f953626f6d2073c73
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-05-09 15:17:27 -0400

    added achievement check

--- features/achievements.js
+++ features/achievements.js
@@ -14,7 +14,11 @@ module.exports = {
     checkAchievements: async function(msg) {
         //Requires a message so that the user can be congratulated if they reach an achievement.
         const data = get_userdata.byId(msg.member.id);
-
+        try {
+            console.log(data.achievements - data.achievements)}
+        catch(e) {
+            data.achievements = []
+        }
         //X Rank Achievement
         if (data.score >= rank.thresholds[6]) {
             if (!data.achievements.includes("Xrank")) {
