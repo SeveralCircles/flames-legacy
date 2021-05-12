f8a3bb15c03e95c08e7c95bc62aa4be3f6a07635
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-05-12 16:46:07 -0400

    fixed uri

--- features/severalcircles.js
+++ features/severalcircles.js
@@ -1,7 +1,7 @@
 const https = require('https');
 function getData(path) {
     const request = require('request');
-    request('severalcircles.com/flames' + path, { json: true }, (err, res, body) => {
+    request('https://severalcircles.com/flames' + path, { json: true }, (err, res, body) => {
       if (err) { return console.log(err); }
       console.log(body);
       return body;
