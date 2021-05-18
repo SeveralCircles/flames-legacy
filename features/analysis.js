413a2ce8b8914e51749106a6d49b8b70d6a96244
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-05-18 17:56:46 -0400

    pgfdhjsh she platform

--- features/analysis.js
+++ features/analysis.js
@@ -45,10 +45,10 @@ module.exports = {
       if (entity.metadata && entity.metadata.wikipedia_url) {
       console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}`);
     }
-    if (isPresent(ent, entity.name)) {
+    // if (isPresent(ent, entity.name)) {
       ent[findIndex(ent, entity.name)][1] += Math.round(entity.salience * 1000); 
-    }
-    else ent.push(entity.name);
+    // }
+    ent.push(entity.name);
     return ent;
   });
   // ent.sort((a, b) => a[1] - b[1]);
