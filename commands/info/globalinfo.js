2c9711d99d75ec79115c0196df2c54dd063d675b
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-05-19 16:21:14 -0400

    Added Flames Data

--- commands/info/globalinfo.js
+++ commands/info/globalinfo.js
@@ -6,6 +6,7 @@ const rank = require("../../features/rank")
 const info = require ("../../features/info")
 const get_globaldata = require("../../data/get_globaldata")
 const ulist = require("../../data/ulist.json")
+const flamesdata = require("../../data/flamesdata.json");
 module.exports = class GlobalInfoCommand extends commando.Command {
 	constructor(client) {
 		super(client, {
@@ -37,7 +38,7 @@ module.exports = class GlobalInfoCommand extends commando.Command {
         .addField("F Rank Threshold", rank.thresholds[0], true)
         // .addField("Top Score", rank.scores[rank.scores.length-1])
         .setColor("DARK_VIVID_PINK")
-        .setFooter("Flames", this.client.user.displayAvatarURL());
+        .setFooter("Flames ver. " + flamesdata.version, this.client.user.displayAvatarURL());
         let percent = 0
         if (up){
             percent = (Math.round(10000 * ((gdata.score - (gdata.score - gdata.dailyChange))/Math.abs(gdata.score - gdata.dailyChange))))/100;
