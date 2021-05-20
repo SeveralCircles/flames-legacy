2a8ac08bfdd1c547a2ee7a207da8181b5ca10fd4
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-05-19 20:25:34 -0400

    i igive up for tonight

--- commands/info/shop.js
+++ commands/info/shop.js
@@ -1,65 +1,61 @@
-const commando = require('discord.js-commando');
-const Discord = require('discord.js');
-const get_userdata = require("../../data/get_userdata")
-const get_guilddata = require("../../data/get_guilddata")
-const rank = require("../../features/rank")
-const info = require ("../../features/info");
-const message = require('../../events/message');
-const achievements = require("../../features/achievements")
-const get_globaldata = require ("../../data/get_globaldata")
-const analysis = require("../../features/analysis")
-const gamerpoints = require("../../features/gamerpoints");
-const { exchangeRate } = require('../../features/gamerpoints');
-const { userDataCorrupt } = require('../../features/info');
-const success = async function(member, message, multiplierCost, client) {
-    console.log(await gamerpoints.purchaseDialog(member, message, multiplierCost, "Multiplier Increase", client));
-    return false;
-                    
-}
-module.exports = class ShopCommand extends commando.Command {
-	constructor(client) {
-		super(client, {
-            name: 'shop',
-            aliases: ['upgrades'],
-			group: 'info',
-			memberName: 'shop',
-			description: 'Allows you to spend your Gamer Points.',
-            guildOnly: true
-            });  
-}
+// const commando = require('discord.js-commando');
+// const Discord = require('discord.js');
+// const get_userdata = require("../../data/get_userdata")
+// const get_guilddata = require("../../data/get_guilddata")
+// const rank = require("../../features/rank")
+// const info = require ("../../features/info");
+// const message = require('../../events/message');
+// const achievements = require("../../features/achievements")
+// const get_globaldata = require ("../../data/get_globaldata")
+// const analysis = require("../../features/analysis")
+// const gamerpoints = require("../../features/gamerpoints");
+// const { exchangeRate } = require('../../features/gamerpoints');
+// const { userDataCorrupt } = require('../../features/info');
+// module.exports = class ShopCommand extends commando.Command {
+// 	constructor(client) {
+// 		super(client, {
+//             name: 'shop',
+//             aliases: ['upgrades'],
+// 			group: 'info',
+// 			memberName: 'shop',
+// 			description: 'Allows you to spend your Gamer Points.',
+//             guildOnly: true
+//             });  
+// }
     
-    async run(msg) {
-        var succid = Math.random();
-        var data = get_userdata.byId(msg.member);
-        var multiplierCost = Math.round(100 * data.multiplier);
-        if (data.multiplier == undefined) data.multiplier = 1.0;
-        let embed = new Discord.MessageEmbed()
-        .setAuthor("Flames Shop", msg.member.user.displayAvatarURL())
-        .setTitle(msg.member.displayName + ", here are the available upgrades right now.")
-        .setDescription("React with the one you wish to purchase.")
-        .addField("1️⃣: Multiplier Upgrade (" + multiplierCost + "GP)", "Increases how much your Flames Score changes with each message, positive or negative, by +.1x. \nYour current multiplier: " + data.multiplier + "x")
-        .setTimestamp()
-        .setFooter("Flames | 🔴 to cancel.", this.client.user.displayAvatarURL());
-        var message = await msg.channel.send(embed)
-        message.react('1️⃣').then(r => {
-            message.react('🔴');
-    });
+//     async run(msg) {
+//         var succid = Math.random();
+//         var data = get_userdata.byId(msg.member);
+//         var multiplierCost = Math.round(100 * data.multiplier);
+//         if (data.multiplier == undefined) data.multiplier = 1.0;
+//         let embed = new Discord.MessageEmbed()
+//         .setAuthor("Flames Shop", msg.member.user.displayAvatarURL())
+//         .setTitle(msg.member.displayName + ", here are the available upgrades right now.")
+//         .setDescription("React with the one you wish to purchase.")
+//         .addField("1️⃣: Multiplier Upgrade (" + multiplierCost + "GP)", "Increases how much your Flames Score changes with each message, positive or negative, by +.1x. \nYour current multiplier: " + data.multiplier + "x")
+//         .setTimestamp()
+//         .setFooter("Flames | 🔴 to cancel.", this.client.user.displayAvatarURL());
+//         var message = await msg.channel.send(embed)
+//         message.react('1️⃣').then(r => {
+//             message.react('🔴');
+//     });
 
-    // First argument is a filter function
-    message.awaitReactions((reaction, user) => user.id == msg.author.id && (reaction.emoji.name == '1️⃣' || reaction.emoji.name == '🔴'),
-            { max: 1, time: 30000 }).then(collected => {
-                    if (collected.first().emoji.name == '1️⃣') {
-                        if(success(msg.member, message, multiplierCost, this.client)) {
-                            if (get_userdata.byId(msg.member.id).succid != succid) return;
-                            data.multiplier += .1;
-                            get_userdata.writeById(msg.member.id, data);
-                        } else return;
-                    }
-                    else
-                            message.edit('Operation canceled.');
-            }).catch(e => {
-                    console.log(e);
-                    message.edit('Operation expired.');
-            });
-    }
-}
+//     // First argument is a filter function
+//     message.awaitReactions(async function(reaction, user)
+//             { max: 1, time: 30000 }).then(collected => {
+//                     if (collected.first().emoji.name == '1️⃣') {
+//                         if(gamerpoints.purchaseDialog(member, message, 100, "Multiplier Increase", client, succid)) {
+                            
+//                             if (get_userdata.byId(msg.member.id).succid != succid) return;
+//                             data.multiplier += .1;
+//                             get_userdata.writeById(msg.member.id, data);
+//                         } else return;
+//                     }
+//                     else
+//                             message.edit('Operation canceled.');
+//             }).catch(e => {
+//                     console.log(e);
+//                     message.edit('Operation expired.');
+//             });
+//     }
+// }
