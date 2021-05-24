2e4c4ad32cfac581d94440ebd5380973e5ea0fc5
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-05-24 14:14:42 -0400

    Optimization continues

--- data/get_userdata.js
+++ data/get_userdata.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const defaults = require("./userdefault.json")
 var ulist = null;
-const currentVersion = 1;
 module.exports = {
     byId: function(id) {
         try {
@@ -24,18 +23,4 @@ module.exports = {
         fs.writeFileSync("./data/" + id + ".json", JSON.stringify(json));
     },
     defaults: defaults,
-    update: async function(id, json) {
-        let data2 = null;
-        if (this.byId(id).version < 1 || this.byId(id).version == null) {
-            data2 = this.defaults;
-            data2.firstSeen = json.firstSeen;
-            data2.verified = json.verified;
-            data2.score = json.score;
-            data2.averageSentiment = json.averageSentiment;
-            data2.streak = 0;
-            data2.lastStreak = -2;
-            data2.version = 1;
-        } else (data2 = json)
-        return data2;
-    }
 }
