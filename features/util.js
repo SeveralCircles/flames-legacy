a890d601ab193e12a48bb8c4a6bfb5c5c85cf25f
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-05-24 13:29:00 -0400

    added double friday

--- features/util.js
+++ features/util.js
@@ -26,9 +26,11 @@ module.exports = {
         else return remain + " days"
     },
     getDailyMessage: function(data, name) {
+        let date = new Date();
         if (data.streak % 365 == 0) return "Incredible, " +  name + "! You've used Flames every single day for the past " + (data.streak / 365) + "year(s)! That is truly amazing. Thanks for using Flames.";
         else if (data.streak % 30 == 0) return "Wow, you've used Flames every day for the last " + (data.streak / 30) + "month(s)! Nice job.";
         else if (data.streak % 7 == 0) return "Congratulations on your " + (data.streak / 7) + "week streak.";
+        else if (date.getDay() == 5) return "It's Double Friday, so all message scores are multiplied by 2 today!"
         else {
             let msgs = ["What's up?", "Nice to see you again!", "Keep it 💯"]
             if (rank.getRank(data.score) != "X") msgs.push("Maybe today's the day you finally get to X rank!")
