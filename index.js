474b8bd57a2bde01ec9f6611249237a923645ce8
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-05-24 16:09:23 -0400

    pray for me

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ console.log("Updating ranking information");
 rank.sync();
 
 console.log("Scheduling tasks...")
-const updateJob = schedule.scheduleJob('0 * * * *', rank.sync()); // Also schedule a job to update rank information every hour.
-const newDay = schedule.scheduleJob('0 * * *', console.log("It's a brand new day, let's make the most of it!")); // Just for fun
+const updateJob = schedule.scheduleJob('0 * * * *', function() {rank.sync()}); // Also schedule a job to update rank information every hour.
+const newDay = schedule.scheduleJob('0 * * *', function(){console.log("It's a brand new day, let's make the most of it!")}); // Just for fun
 console.log("Logging in...")
 bot.login();
 console.log("Logged in.");
