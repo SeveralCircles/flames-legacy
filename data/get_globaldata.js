6041dbac9410179ab4ec1e9ed74dc1f86c5d7e3f
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-05-24 16:05:01 -0400

    removed records from globaldata

--- data/get_globaldata.js
+++ data/get_globaldata.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const defaults = require("./globaldefault.json")
-const recorddefaults = require("./recordsdefault.json")
+// const recorddefaults = require("./recordsdefault.json")
 // var ulist = null;
 module.exports = {
     getValues: function() {
@@ -21,22 +21,22 @@ module.exports = {
         fs.writeFileSync("./data/" + "global" + ".json", JSON.stringify(json));
     },
     defaults: defaults,
-    getRecordValues: function() {
-        try {
-            // console.log(id)
-            var raw = fs.readFileSync("./data/" + "global" + ".json")
-            // console.log(raw)
-        } catch (e) {
-            console.log("File not found or invalid (records.json), writing defaults.")
-            let data = JSON.stringify(recorddefaults);
-            fs.writeFileSync("./data/" + "records" + ".json", data);
-            return defaults;
-        }
-        let json = JSON.parse(raw); 
-        return json;
-    },
-    writeRecordValues: function(json) {
-        fs.writeFileSync("./data/" + "records" + ".json", JSON.stringify(json));
-    },
-    recorddefaults: recorddefaults
+    // getRecordValues: function() {
+    //     try {
+    //         // console.log(id)
+    //         var raw = fs.readFileSync("./data/" + "global" + ".json")
+    //         // console.log(raw)
+    //     } catch (e) {
+    //         console.log("File not found or invalid (records.json), writing defaults.")
+    //         let data = JSON.stringify(recorddefaults);
+    //         fs.writeFileSync("./data/" + "records" + ".json", data);
+    //         return defaults;
+    //     }
+    //     let json = JSON.parse(raw); 
+    //     return json;
+    // },
+    // writeRecordValues: function(json) {
+    //     fs.writeFileSync("./data/" + "records" + ".json", JSON.stringify(json));
+    // },
+    // recorddefaults: recorddefaults
 }
